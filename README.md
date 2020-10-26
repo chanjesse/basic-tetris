2e0db08053c1d2cee519aee2392cb93cc891c1e3
chanjesse <jesse.tk.chan@gmail.com>
2020-10-25 21:47:25 -0700

    worked on app.js: defined squares (which is an array from .grid div), connected to html's score and html's start button

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 basic tetris using javascript, html, css
 
 will be using modified tetrominos
+
+--stopped at 27:39 of video--
--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
+  //talks to the grid element in the html file
+  const grid = document.querySelector('.grid')
+
+  //to have js talk to all 200 squares
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+
+  //connects to html's ID of "score"
+  const ScoreDisplay = document.querySelector('#score')
+
+  //connects to html's ID of "start-button"
+  const StartBtn = document.querySelector('#start-button')
+
+  const width = 10
 })
