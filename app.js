faed7a5948ac11f29db7a27d3448651967dc9a34
chanjesse <jesse.tk.chan@gmail.com>
2020-11-06 20:08:38 -0800

    added draw, undraw, movedown, and freeze functions

--- app.js
+++ app.js
@@ -52,14 +52,53 @@ document.addEventListener('DOMContentLoaded', () => {
   const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
   let currentPosition = 4
-  let current = theTetrominoes[0][0]
+  let currentRotation = 0
 
-  //draw the first rotation in the first tetromino
+  // randomly select a tetromino and its first rotation
+  let random = Math.floor(Math.random()*theTetrominoes.length)
+
+  let current = theTetrominoes[random][currentRotation]
+
+  // draw the Tetromino
   function draw() {
     current.forEach(index => {
       squares[currentPosition + index].classList.add('tetromino')
     })
   }
 
-  draw()
+  // undraw the Tetrominoes
+  function undraw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.remove('tetromino')
+    })
+  }
+
+  //make the tetromino move down every second
+  timerId = setInterval(moveDown, 500)
+
+  //move down function
+  function moveDown() {
+    undraw()
+    currentPosition += width
+    draw()
+    freeze()
+  }
+
+  //freeze function
+  function freeze() {
+    // if tetromino hits taken, then begin adding taken tag to the board
+    if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+      current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+
+      //start a new tetromino falling
+      random = Math.floor(Math.random() * theTetrominoes.length)
+      current = theTetrominoes[random][currentRotation]
+      currentPosition = 4
+      draw()
+    }
+  }
+
+
+
+
 })
