8a0ca3a1e8beea1cb2dfa4b7fdf6c67814f56f3c
chanjesse <jesse.tk.chan@gmail.com>
2020-11-08 20:35:48 -0800

    added moveLeft and moveRight functions

--- app.js
+++ app.js
@@ -76,6 +76,21 @@ document.addEventListener('DOMContentLoaded', () => {
   //make the tetromino move down every second
   timerId = setInterval(moveDown, 500)
 
+  //assign functions to keyCodes
+  function control(e) {
+    if(e.keyCode === 37) {
+      moveLeft()
+    } else if (e.keyCode === 38) {
+      //rotate
+    } else if(e.keyCode === 39) {
+      moveRight()
+    } else if(e.keyCode === 40) {
+      moveDown()
+    }
+  }
+  //could use keyup as well
+  document.addEventListener('keydown', control)
+
   //move down function
   function moveDown() {
     undraw()
@@ -98,6 +113,34 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  // move the tetromino left, unless is at the edge or there is a blockage
+  function moveLeft() {
+    undraw()
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+
+    // if not at left edge, then can move
+    if(!isAtLeftEdge) currentPosition -=1
+
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition += 1
+    }
+
+    draw()
+  }
+
+  // move the tetromino right, unless is at the edge or there is a blockage
+  function moveRight() {
+    undraw()
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1)
+
+    if(!isAtRightEdge) currentPosition +=1
+
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition -=1
+    }
+
+    draw()
+  }
 
 
 
